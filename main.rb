11e12297033dd6fac920f5f7271d8cf166c4bdb6
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-06-11 13:56:49 +0900

    multi asp site

--- main.rb
+++ main.rb
@@ -1,34 +1,56 @@
 require "mechanize"
 require "csv"
+require "pry"
 
 asp_info = YAML.load_file("asp.yml")
 
-# For example: A8
-ASP_LOGIN_PAGE = "https://www.a8.net"
-ASP_DATA_PAGE = "https://pub.a8.net/a8v2/asResultReportAction.do?action=dm"
-LOGIN_ID = asp_info["asp"]["a8"]["id"]
-PASSWORD = asp_info["asp"]["a8"]["password"]
+# Settings
+asp_info = YAML.load_file("asp.yml")
+security = YAML.load_file("security.yml")
+USER_AGENT = "Windows Mozilla"
+# AFFILIATES = ["a8", "felmat", "access_trade", "amazon_associate", "mosimo", "rentrax"]
+AFFILIATES = ["a8"]
 
 agent = Mechanize.new
 agent.user_agent = "Windows Mozilla"
-agent.get(ASP_LOGIN_PAGE) do |page|
-  form = page.form_with(name: "asLogin") do |f|
-    f.login = LOGIN_ID
-    f.passwd = PASSWORD
-  end
-  form.submit
 
-  _page = agent.get(ASP_DATA_PAGE)
-  table = _page.search(".reportTable1")
-  latest_data = table.search("tr")[1]
-  date = latest_data.search("td")[0].text.gsub(/\r\n|\r|\n|\s|\t/, "")
-  confirmed_count = latest_data.search("td")[1].text.gsub(/\r\n|\r|\n|\s|\t/, "")
-  tax_included = latest_data.search("td")[2].text.gsub(/\r\n|\r|\n|\s|\t/, "")
-  tax_excluded = latest_data.search("td")[3].text.gsub(/\r\n|\r|\n|\s|\t/, "")
-  tax = latest_data.search("td")[4].text.gsub(/\r\n|\r|\n|\s|\t/, "")
+# Prepare csv template
+CSV.open("data.csv", "w") do |csv|
+  csv << ["アフィリエイトサイト", "本日発生件数", "本日発生報酬", "本日承認件数", "本日承認報酬", "当月発生件数", "当月発生報酬", "当月承認件数", "当月承認報酬"]
+end
 
-  CSV.open("data.csv", "w") do |csv|
-    csv << ["日付", "確定件数", "確定報酬額・税込", "確定報酬額・税別", "確定報酬額・税金"]
-    csv << [date, confirmed_count, tax_included, tax_excluded, tax]
+AFFILIATES.each do |affiliate|
+  login_page = asp_info[affiliate]["login"]
+  data_page  = asp_info[affiliate]["data"]
+
+  login_id = security[affiliate]["id"]
+  password = security[affiliate]["password"]
+
+  agent.get(login_page) do |page|
+    case affiliate
+    when "a8"
+      form = page.form_with(name: "asLogin") do |f|
+        f.login = login_id
+        f.passwd = password
+      end
+      form.submit
+
+      _page = agent.get(data_page)
+      table = _page.search(".reportTable1")
+      latest_data = table.search("tr")[1]
+      confirmed_count = latest_data.search("td")[1].text.gsub(/\r\n|\r|\n|\s|\t/, "").gsub(/[^\d]/, "")
+      confirmed_reward = latest_data.search("td")[2].text.gsub(/\r\n|\r|\n|\s|\t/, "").gsub(/[^\d]/, "")
+      CSV.open("data.csv", "a") do |csv|
+        csv << [affiliate, "-", "-", "-", "-", "-", "-", confirmed_count, confirmed_reward]
+      end
+    when "felmat"
+    when "access_trade"
+    when "amazon_associate"
+    when "mosimo"
+    when "rentrax"
+    when "presco"
+    else
+      raise "対応していません"
+    end
   end
 end
